3c0c09999419f7397122b62dfdc45fb113e661a0
akumi07 <akash9338144396@gmail.com>
2024-12-26 23:44:58 +0530

    day 4

--- C_Preparing_for_the_Exam.cpp
+++ C_Preparing_for_the_Exam.cpp
@@ -4,6 +4,34 @@ int main(){
 
 int t;
 cin>>t;
-while(t--){}
+while(t--){
+    int n,m,k;
+    cin>>n>>m>>k;
+    if(m==k){
+        cout<<"1111"<<endl;
+        continue;
+    }
+    vector<int>v(m);
+    for(int i=0;i<m;i++){
+        cin>>v[i];
+    }
+    set<int>st;
+    for(int i=0;i<k;i++){
+        int x;
+        cin>>x;
+        st.insert(x);
+    }
+    string res="";
+    for(int i=0;i<n;i++){
+        if(st.find(v[i])!=st.end()){
+            res+="0";
+        }
+        else{
+            res+="1";
+        }
+    }
+    cout<<res<<endl;
+
+}
 return 0 ;
 }
